7f031bad9bff73ff6e693932155685dc78f49a3b
Harish Devathraj <devathrajharish@gmail.com>
2020-09-12 02:33:15 -0500

    new spark

--- sparktest.py
+++ sparktest.py
@@ -10,42 +10,47 @@ def main():
     #     .appName("Word Count") \
     #     .config("spark.some.config.option", "some-value") \
     #     .getOrCreate()
+    sc = SparkContext("local", "PySpark Word Count Exmaple")
+    la = sc.textFile('./Dataset/sample.txt')
+    words = la.flatMap(lambda word: word.lower().split(" "))
+    rdd = words.map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
+    rdd.map(lambda (k,v): (v,k)).sortByKey(False)
+    rdd.saveAsTextFile("sample_counts.txt")
 
-    # l = [('Alice', 1)]
-    # rdd = spark.createDataFrame(l).collect()
-    # print(rdd)
-    # sc = SparkContext('local')
 
-    config = configparser.ConfigParser()
-
-    config.read_file(open('../config.cfg'))
-    os.environ['PYSPARK_PYTHON'] = '/usr/bin/python3'
-    os.environ['PYSPARK_DRIVER_PYTHON'] = '/usr/bin/python3'
-
-    os.environ['AWS_ACCESS_KEY_ID'] = config['AWS']['AWS_ACCESS_KEY_ID']
-    os.environ['AWS_SECRET_ACCESS_KEY'] = config['AWS']['AWS_SECRET_ACCESS_KEY']
-
-    country_data_path = config['S3']['COUNTRY_DATA']
-    # country_notes_path = config['S3']['COUNTRY_NOTES_DATA']
-    # foot_notes_path = config['S3']['FOOT_NOTES_DATA']
-    series_path = config['S3']['SERIES_DATA']
-    # series_notes_path = config['S3']['SERIES_NOTES_DATA']
-    indicators_path = config['S3']['INDICATORS_DATA']
-    # output_path = config['S3']['OUTPUT']
-    print(indicators_path)
-
-    sc = SparkSession.builder.config('spark.jars.packages',
-                                        'org.apache.hadoop:hadoop-aws:2.6.4').getOrCreate()
-
-    sc._jsc.hadoopConfiguration().set("fs.s3a.access.key",
-                                         os.environ['AWS_ACCESS_KEY_ID'])
-    sc._jsc.hadoopConfiguration().set("fs.s3a.secret.key",
-                                         os.environ['AWS_SECRET_ACCESS_KEY'])
-    sc._jsc.hadoopConfiguration().set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-    indicators_data = sc.read.csv(indicators_path, header=True)
-    country_data = sc.read.csv(country_data_path, header=True)
-    series_data = sc.read.csv(series_path, header=True)
-    print(indicators_data.head(5))
+    sc.stop()
 
 if __name__ == '__main__':
     main()
+
+
+    # config = configparser.ConfigParser()
+    #
+    # config.read_file(open('../config.cfg'))
+    # os.environ['PYSPARK_PYTHON'] = '/usr/bin/python3'
+    # os.environ['PYSPARK_DRIVER_PYTHON'] = '/usr/bin/python3'
+
+    # os.environ['AWS_ACCESS_KEY_ID'] = config['AWS']['AWS_ACCESS_KEY_ID']
+    # os.environ['AWS_SECRET_ACCESS_KEY'] = config['AWS']['AWS_SECRET_ACCESS_KEY']
+    #
+    # country_data_path = config['S3']['COUNTRY_DATA']
+    # # country_notes_path = config['S3']['COUNTRY_NOTES_DATA']
+    # # foot_notes_path = config['S3']['FOOT_NOTES_DATA']
+    # series_path = config['S3']['SERIES_DATA']
+    # # series_notes_path = config['S3']['SERIES_NOTES_DATA']
+    # indicators_path = config['S3']['INDICATORS_DATA']
+    # # output_path = config['S3']['OUTPUT']
+    # print(indicators_path)
+    #
+    # sc = SparkSession.builder.config('spark.jars.packages',
+    #                                     'org.apache.hadoop:hadoop-aws:2.6.4').getOrCreate()
+    #
+    # sc._jsc.hadoopConfiguration().set("fs.s3a.access.key",
+    #                                      os.environ['AWS_ACCESS_KEY_ID'])
+    # sc._jsc.hadoopConfiguration().set("fs.s3a.secret.key",
+    #                                      os.environ['AWS_SECRET_ACCESS_KEY'])
+    # sc._jsc.hadoopConfiguration().set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+    # indicators_data = sc.read.csv(indicators_path, header=True)
+    # country_data = sc.read.csv(country_data_path, header=True)
+    # series_data = sc.read.csv(series_path, header=True)
+    # print(indicators_data.head(5))
