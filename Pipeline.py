c7d613749ca24f91bb5bf819dacdaddf2c437594
Harish Devathraj <devathrajharish@gmail.com>
2020-10-06 00:44:56 -0500

    new pipeline

---
+++ Pipeline.py
@@ -0,0 +1,35 @@
+from flask import Flask, Response, stream_with_context
+import time
+import uuid
+import random
+
+# app = Flask(__name__)
+#
+# from flask import Flask
+
+app = Flask(__name__)
+
+
+# @app.route("/")
+# def home():
+    # return "Hello, World!"
+@app.route("/very_large_request/<int:rowcount>", methods=["GET"])
+def get_large_request(rowcount):
+    """retunrs N rows of data"""
+
+    def f():
+        """The generator of mock data"""
+        for _i in range(rowcount):
+            time.sleep(.01)
+            txid = uuid.uuid4()
+            print(txid)
+            uid = uuid.uuid4()
+            amount = round(random.uniform(-1000, 1000), 2)
+            yield f"('{txid}', '{uid}', {amount})\n"
+    return Response(stream_with_context(f()))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
